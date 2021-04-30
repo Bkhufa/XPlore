bab29d748712fff8132093344c70ca2469866dd7
Bryan Khufa <bryankhufar@gmail.com>
2021-04-30 10:29:39 +0700

    Initial Commit

---
+++ XPlore/XPloreApp.swift
@@ -0,0 +1,17 @@
+//
+//  XPloreApp.swift
+//  XPlore
+//
+//  Created by Bryan Khufa on 30/04/21.
+//
+
+import SwiftUI
+
+@main
+struct XPloreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
